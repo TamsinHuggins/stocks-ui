d40d406df46fb5ed5c54de5a1ea7b831228d18ea
Tamsin Huggins <tamsin.hns@gmail.com>
2024-07-18 13:41:00 +0100

    removed some junk from App

--- react-stocks-demo/src/App.jsx
+++ react-stocks-demo/src/App.jsx
@@ -1,21 +1,12 @@
 import "./App.css";
 import PurchaseStock from "./components/PurchaseStock/PurchaseStock";
 import OrderTable from "./components/OrderTable/OrderTable";
-import { useState } from "react";
-import { buyDummyStock } from "./data/dummyData";
 
 function App() {
-  const [bought, setBought] = useState(0); // currently can only be used once per page load
-
-  const handleBuy = (ticker, quantity) => {
-    setBought(1);
-    buyDummyStock(ticker, quantity);
-  };
-
   return (
     <div>
       <OrderTable />
-      <PurchaseStock buyFunc={handleBuy} />
+      <PurchaseStock />
     </div>
   );
 }
